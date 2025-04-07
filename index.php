2433c6939fb44fdeff684902a569c2cb7110b606
Jeonggeun Jin <pion0926@gmail.com>
2025-04-08 01:50:59 +0900

    index change front

--- index.php
+++ index.php
@@ -29,11 +29,11 @@
                         <div class="textCont">
                             <div class="report-header">
                                 <h2 class="report-title">투명하고 효율적인 재정관리</h2>
-                                <p class="report-subtitle">투명성과 신뢰는 글로벌헬스파트너스의 핵심 가치 입니다.</p>
+                                <p class="report-subtitle"><span style="font-weight: bold; color: #0066cc;">투명성과 신뢰</span>는 글로벌헬스파트너스의 핵심 가치 입니다.</p>
                             </div>
                             <div class="report-description">
                                 <p></p>
-                                <p>모든 수입과 지출을 투명하게 관리하며,<br/>활동 목적에 따라 꼭 필요한 곳에 효율적으로 재정을 사용하고 있습니다.</p>
+                                <p>소중한 후원금이 꼭 필요한 곳에 효율적으로 사용될 수 있도록, 모든 수입과 지출을 투명하게 관리하고 있습니다.</p>
                             </div>
                             <div class="report-links">
                                 <a href="/web_basic/introduce/introduce.php?pagen=448" class="report-link">
@@ -51,7 +51,7 @@
                                 </div>
                                 <div class="graph-text">
                                     <p class="highlight">글로벌헬스파트너스는</p>
-                                    <p class="emphasis">후원금의 80% 이상을 사업비로 사용합니다.</p>
+                                    <p class="emphasis">투명성과 신뢰의 가치를 지향합니다</p>
                                 </div>
                             </div>
                         </div>
